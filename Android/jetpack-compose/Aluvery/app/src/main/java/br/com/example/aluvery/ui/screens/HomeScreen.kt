ee445e1c6767b073cc89f92170585cecc21d9af8
José Luiz R C Júnior <j.luiz.junior26@gmail.com>
2025-07-01 15:27:51 -0300

    Implementado by remove a necessidade de usar .value na variavel, remember mutableStateOf() mantém e altera o estado da variavel, OutlineTextField campo digitar texto

--- Android/jetpack-compose/Aluvery/app/src/main/java/br/com/example/aluvery/ui/screens/HomeScreen.kt
+++ Android/jetpack-compose/Aluvery/app/src/main/java/br/com/example/aluvery/ui/screens/HomeScreen.kt
@@ -1,13 +1,22 @@
 package br.com.example.aluvery.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.example.aluvery.model.Product
+import br.com.example.aluvery.sampledata.sampleSections
 import br.com.example.aluvery.ui.components.ProductsSection
 
 
@@ -15,21 +24,35 @@ import br.com.example.aluvery.ui.components.ProductsSection
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    LazyColumn(
-        Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(vertical = 16.dp)
-    ) {
-        for (section in sections) {
-            val title = section.key
-            val products = section.value
-            item {
-                ProductsSection(
-                    title = title,
-                    products = products
-                )
+    Column{
+        var text by remember { mutableStateOf("Produto") }
+        OutlinedTextField(value = text, onValueChange = { newValue ->
+            text = newValue
+        }, Modifier.fillMaxWidth())
+        LazyColumn(
+            Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductsSection(
+                        title = title,
+                        products = products
+                    )
+                }
             }
         }
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen(
+        sections = sampleSections
+    )
 }
