b4eb8d340bc981ddbe7f53d5220fe4966ee113b5
José Luiz R C Júnior <j.luiz.junior26@gmail.com>
2025-07-01 15:52:23 -0300

    Configuração do componente de busca, slots api espaço reservado para icones, titulos e ações

--- Android/jetpack-compose/Aluvery/app/src/main/java/br/com/example/aluvery/ui/screens/HomeScreen.kt
+++ Android/jetpack-compose/Aluvery/app/src/main/java/br/com/example/aluvery/ui/screens/HomeScreen.kt
@@ -5,8 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,10 +31,25 @@ fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
     Column{
-        var text by remember { mutableStateOf("Produto") }
-        OutlinedTextField(value = text, onValueChange = { newValue ->
-            text = newValue
-        }, Modifier.fillMaxWidth())
+        var text by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newValue ->
+                text = newValue
+            },
+            Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp).fillMaxWidth(),
+            shape = RoundedCornerShape(100),
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = "Ícone de lupa")
+            },
+            label = {
+                Text(text = "Produto")
+            },
+            placeholder = {
+                Text(text = "O que você procura?")
+            },
+        )
         LazyColumn(
             Modifier
                 .fillMaxSize(),
